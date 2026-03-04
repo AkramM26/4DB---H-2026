47f205028f68171062b3b7fa0c73ad4059be359d
Mouhamed Akram Ismaila <2415002@etu.cegepsth.qc.ca>
2026-03-04 13:43:46 -0500

    Début GameServices

---
+++ HugoLandConquest/HugoLand.Core/Services/GameService.cs
@@ -0,0 +1,46 @@
+﻿using HugoLand.Core.Data;
+using HugoLand.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HugoLand.Core.Services
+{
+    public class GameService
+    {
+        private readonly HugoLandContext _context;
+
+        public GameService(HugoLandContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateGame()
+        {
+            Game game = new();
+            Player player1 = new(), player2 = new();
+            game.Players.Add(player1);
+            game.Players.Add(player2);
+
+            foreach (var p in game.Players)
+            {
+                // Attribuer les éléments de départ à chaque joueur
+            }
+        }
+
+        public void LoadGame()
+        {
+        }
+
+        public void SaveGame()
+        {
+        }
+
+        public void EndTurn()
+        {
+
+        }
+    }
+}
