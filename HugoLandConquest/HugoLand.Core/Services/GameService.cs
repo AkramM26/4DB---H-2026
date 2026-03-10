ddd93740b1c675a45a1f7f4037ac7e130b72ce08
Mouhamed Akram Ismaila <2415002@etu.cegepsth.qc.ca>
2026-03-10 12:30:34 -0400

    Utilisation des objets crées par le seed pour CreateGame

--- HugoLandConquest/HugoLand.Core/Services/GameService.cs
+++ HugoLandConquest/HugoLand.Core/Services/GameService.cs
@@ -1,5 +1,6 @@
 ﻿using HugoLand.Core.Data;
 using HugoLand.Core.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,10 @@ namespace HugoLand.Core.Services
         public async Task CreateGameAsync()
         {
             await Seed.SeedGameAsync(Context);
+            // Créer la partie en se servant des objets créer par SeedGameAsync
+            var game = Context.Games.FirstOrDefaultAsync();
+            var player1 = Context.Players.FirstOrDefaultAsync(p => p.PlayerNumber == 1);
+            var player2 = Context.Players.FirstOrDefaultAsync(p => p.PlayerNumber == 2);
         }
 
         public void LoadGame()
