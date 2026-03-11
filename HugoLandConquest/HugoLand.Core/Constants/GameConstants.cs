12a7ca0f8bd74c7fa72474fe3af6223c34911111
memilep <emilepotvin09@gmail.com>
2026-03-11 12:20:38 -0400

    Rajout barebone du InstallationService

--- HugoLandConquest/HugoLand.Core/Constants/GameConstants.cs
+++ HugoLandConquest/HugoLand.Core/Constants/GameConstants.cs
@@ -9,5 +9,7 @@ namespace HugoLand.Core.Constants
     public static class GameConstants
     {
         public const int energyRecuperation = 2;
+        public const int CampConstructionCost = 20;
+        public const int MinimumArmyForceForActions = 10;
     }
 }
---
+++ HugoLandConquest/HugoLand.Core/Services/InstallationService.cs
@@ -0,0 +1,51 @@
+﻿using HugoLand.Core.Constants;
+using HugoLand.Core.Data;
+using HugoLand.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HugoLand.Core.Services
+{
+    public class InstallationService(HugoLandContext context)
+    {
+        private readonly HugoLandContext Context = context;
+
+        public async Task<bool> BuildCampAsync(Guid militaryDetachmentId)
+        {
+            var militaryDetachment = await Context.MilitaryDetachments
+                .Include(m => m.Player)
+                .Include(m => m.Territory)
+                .ThenInclude(t => t.Installation)
+                .FirstOrDefaultAsync(m => m.Id == militaryDetachmentId);
+
+            if (militaryDetachment == null)
+                return false;
+
+            if (militaryDetachment.MilitaryForce < GameConstants.MinimumArmyForceForActions)
+                return false;
+
+            if (!militaryDetachment.CanAct)
+                return false;
+
+            if (militaryDetachment.Territory.Installation != null)
+                return false;
+
+            if (militaryDetachment.Player.Gold < GameConstants.CampConstructionCost)
+                return false;
+
+            var camp = Installation.Create(
+                InstallationType.Camp,
+                militaryDetachment.TerritoryId,
+                militaryDetachment.GameId);
+
+            militaryDetachment.Player.Gold -= GameConstants.CampConstructionCost;
+            militaryDetachment.CanAct = false;
+            militaryDetachment.Territory.Installation = camp;
+
+            await Context.Installations.AddAsync(camp);
+            await Context.AddAsync(PlayerAction.Create(militaryDetachment.GameId));
+            await Context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
