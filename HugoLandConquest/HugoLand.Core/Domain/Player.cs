9219a593ce801d1dde4eff8d9a315f5114b49cbf
Samuel Kple-Faget <2377816@etu.cegepsth.qc.ca>
2026-05-14 22:29:29 -0400

    Gestion de problèmes liés aux migrations

--- HugoLandConquest/HugoLand.Core/Domain/Player.cs
+++ HugoLandConquest/HugoLand.Core/Domain/Player.cs
@@ -18,7 +18,7 @@ namespace HugoLand.Core.Domain
         public int TurnInDept { get; set; }
         public Guid GameId { get;  set; }
         public virtual Game Game { get; set; }
-        public List<int> ForcesTable { get; set; }
+        public List<int>? ForcesTable { get; set; } = new List<int>();
         public virtual ICollection<MilitaryDetachment> MilitaryDetachments { get; set; } = [];
         public virtual ICollection<Installation> Installations { get; set; } = [];
 
